cd64b247e0a691b9ceffb483f54d987476cfae4c
unknown <riosh.valeria@gmail.com>
2024-05-17 15:08:46 -0500

    mediator inicial, ejercicio

---
+++ Comportamiento/Mediator/Coche.java
@@ -0,0 +1,21 @@
+public class Coche {
+    private Telefono telefono;
+    private Radio radio;
+
+    public void setTelefono(Telefono telefono){
+        this.telefono = telefono;
+    }
+
+    public void setRadio(Radio radio) {
+        this.radio = radio;
+    }
+
+    public void enciende(){
+        radio.enciende();
+        telefono.apagaMusica();
+    }
+
+    public void apaga(){
+        radio.apaga();
+    }
+}
