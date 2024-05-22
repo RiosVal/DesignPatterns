736e26e36217e47585b97bde9b8c26dc7e257ac4
unknown <riosh.valeria@gmail.com>
2024-05-22 11:45:59 -0500

    Inicializo ejercicio decorator

---
+++ Estructurales/Decorator/classes/LoggerDecorator.java
@@ -0,0 +1,16 @@
+package classes;
+
+import java.util.List;
+
+public class LoggerDecorator implements BaseDatos{
+    @Override
+    public void inserta(String registro) {
+        //implementar
+    }
+
+    @Override
+    public List<String> registros() {
+        //implementar
+        return null;
+    }
+}
