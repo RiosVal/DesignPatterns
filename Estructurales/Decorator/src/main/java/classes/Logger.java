960058512d8051f2cb97e498b6d5f6e84c6f9e08
unknown <riosh.valeria@gmail.com>
2024-05-22 12:21:56 -0500

    Solucion decorator

--- Estructurales/Decorator/src/main/java/classes/Logger.java
+++ Estructurales/Decorator/src/main/java/classes/Logger.java
@@ -1,4 +1,4 @@
-package classes;
+package src.main.java.classes;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ Estructurales/Decorator/src/test/java/TestBaseDatosConLogger.java
@@ -0,0 +1,43 @@
+package src.test.java;
+
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import src.main.java.classes.BaseDatos;
+import src.main.java.classes.BaseDatosMemoria;
+import src.main.java.classes.Logger;
+import src.main.java.classes.LoggerDecorator;
+
+public class TestBaseDatosConLogger {
+
+    private Logger logger = new Logger();
+    private BaseDatos sinLogger = new BaseDatosMemoria();
+
+    private BaseDatos bbdd;
+
+    @Before
+    public void init() {
+        // construir un LoggerDecorator usando la base de datos sinLogger y el logger
+        bbdd = new LoggerDecorator(sinLogger, logger);
+    }
+
+    @Test
+    public void test_logger_decorator() {
+        assertNotNull("falta inicializar bbdd en el metodo init()", bbdd);
+        assertFalse("logger no esta vacio", logger.obtener().contains("inserta entrada"));
+        bbdd.inserta("entrada");
+
+        assertTrue("bbdd no ha recibido entrada", sinLogger.registros().contains("entrada"));
+        assertTrue("no se ha registrado la entrada", logger.obtener().contains("inserta entrada"));
+
+        // lectura con el adapter con logger
+        List<String> registros = bbdd.registros();
+
+        assertTrue("el adapter no tiene la nueva entrada", registros.contains("entrada"));
+        assertTrue("no se ha registrado la lectura", logger.obtener().contains("lectura"));
+    }
+}
